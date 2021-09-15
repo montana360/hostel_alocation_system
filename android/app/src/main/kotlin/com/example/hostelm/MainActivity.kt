a7683134aefdadc7072e1fe63704ea730145b93a
montana360 <dnnssm@gmail.com>
2021-09-15 07:46:36 +0100

    first commit login and register done

---
+++ android/app/src/main/kotlin/com/example/hostelm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hostelm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/studenthome.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class studentHome extends StatefulWidget {
+  studentHome({Key key}) : super(key: key);
+
+  @override
+  _studentHomeState createState() => _studentHomeState();
+}
+
+class _studentHomeState extends State<studentHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: null,
+    );
+  }
+}
